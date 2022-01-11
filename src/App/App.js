eeee0c8ae215100ef21f286727ed281bf1465474
marleigh <volcana@gmail.com>
2022-01-10 16:30:42 -0800

    working with useEffect

--- src/App/App.js
+++ src/App/App.js
@@ -4,29 +4,43 @@ import Main from './Main';
 import charData from '../data.js';
 
 export default function App() {
-const [character, setCharacter] = React.useState(charData);
 
-React.useEffect(() => {
+  const [character, setCharacter] = React.useState(charData);
+  const [data, setData] = React.useState([]);
 
-  async function updateChar() {
+  const characterArray = ["cleric", "monk"];
+
+  //need to call the API and grab class data for each of the provided classes
+
+  React.useEffect(() => {
+      characterArray.forEach(char => {
+          fetch(`https://www.dnd5eapi.co/api/classes/${char}`)
+            .then(res => res.json())
+            .then(data => {
+                setData(prevState => ([
+                ...prevState,
+                data
+                ]))
+            })
+        }); 
+        
+  }, []);
+
+  //so now data has all the API info stored in it - we shouldn't need to make anymore API calls (theoretically - for now)
+  //
+
+  data.forEach(char => {
+      setCharacter(prevState, ({
+        ...prevState,
+        
+      }))
+  })
   
-      const response = await fetch(`https://www.dnd5eapi.co/api/classes/${charData.playerClass}`);
-      const data = response.json();
-      setCharacter(prevState => ({
-          ...prevState,
-          hitDice: data.hit_die
-      }));
-  }
-
-  updateChar();
-}, [character]);
-
-console.log(character)
-  // const classArray = character.map(playerClass => {
-  //   return <Main 
-  //             charClass={playerClass}
-  //           />
-  // })
+    //const classArray = character.map(playerClass => {
+    //   return <Main 
+    //             charClass={playerClass}
+    //           />
+    // })
 
   return (
     <div className="container">
