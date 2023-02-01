4777107478f23c11203f66df8849e2004ccba62a
marleigh <volcana@gmail.com>
2023-02-01 14:52:55 -0800

    grabbing class info now

--- src/App/App.js
+++ src/App/App.js
@@ -1,44 +1,29 @@
 import Header from './Header';
 import React from "react";
 import Main from './Main';
-import charData from '../data/characterData.js';
 
 export default function App() {
 
-  const [character, setCharacter] = React.useState(charData);
+  const [charClass, setCharClass] = React.useState({});
   let apiUrl = "https://www.dnd5eapi.co/api";
 
   React.useEffect(() => {
 
-    async function callAPI(char, index) {
-      let response = await fetch(`${apiUrl}/classes/${character[index].playerClass}`);
-      let data = response.json();
-
-      setCharacter(prevState => ({
-        ...prevState,
-        hitDice: data.hit_die
-      }))
-
-      }
-
-    character.forEach((char,index) => callAPI(char, index));
+     fetch(`${apiUrl}/classes/warlock`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setCharClass(data);
+      })
         
   }, []); 
-
-    // const classArray = character.map(player => {
-    //   return <Main 
-    //             character={player}
-    //             key={player.key}
-    //           />
-    // })
 
   return (
     <div className="container">
       <Header />
       <div className='modules'>
-        {/* {classArray} */}
         <Main 
-          character={character}/>
+          character={charClass}/>
       </div>
     </div>
   );
--- src/App/Main.js
+++ src/App/Main.js
@@ -2,15 +2,9 @@
 import React from "react";
 
 export default function Main(props) {
-    console.log(props)
-    const charDisplay = props.character;
     return (
         <main className="main">
-            Name: {charDisplay.name} <br />
-            Class: {charDisplay.playerClass} <br />
-            Dice: {charDisplay.hitDice} <br/>
-            Race: {charDisplay.race}
-        </main>
+test        </main>
     )
 }
 
